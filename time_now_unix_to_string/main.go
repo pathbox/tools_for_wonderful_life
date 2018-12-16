ec81ccbbd2be754568c92e320cb0e3827b42abc7
pathbox <cary.luo@ucloud.cn>
2018-12-16 13:17:07 +0800

    Now timestamp to time string

---
+++ time_now_unix_to_string/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+var (
+	tp = flag.Int64("tp", time.Now().Unix(), "timestamp value")
+)
+
+func main() {
+	flag.Parse()
+	t := *tp
+	r := timeNowUnixToString(t)
+	fmt.Println(r)
+}
+
+func timeNowUnixToString(timestamp int64) string {
+	tm := time.Unix(timestamp, 0)
+	// fmt.Println(tm.Format("2006-01-02 03:04:05 PM")) // 两种格式化模式
+	// fmt.Println(tm.Format("02/01/2006 15:04:05 PM"))
+	s := tm.Format("2006-01-02 15:04:05")
+	return s
+}
+
+// go run main.go 获得当前now的时间
+// go run main.go -tp=1 获得指定时间戳的时间表达式
